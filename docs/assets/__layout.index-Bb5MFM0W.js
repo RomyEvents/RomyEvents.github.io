04988ac483bb5e78467fdf2f0fdc2321e729fe4c
zaaoak <zaaoak@gmail.com>
2025-07-24 03:44:00 +0800

    add docs folder

---
+++ docs/assets/__layout.index-Bb5MFM0W.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CfTQk4ve.js";const e=function(){return o.jsx("div",{children:"Login"})};export{e as component};
---
+++ docs/assets/index-C9r2GTLH.js
@@ -0,0 +1,1 @@
+import{j as t,N as n}from"./index-CfTQk4ve.js";const s=function(){return t.jsx(n,{to:"/landing"})};export{s as component};
